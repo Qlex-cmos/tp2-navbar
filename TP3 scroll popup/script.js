320fce2787caecccd71781b08862129ae3548b7d
Qlex-cmos <alexcheula@gmail.com>
2024-11-16 23:36:23 +0100

    gestion popup et image

--- TP3 scroll popup/script.js
+++ TP3 scroll popup/script.js
@@ -1,9 +1,42 @@
 // Créer un événement au scroll
 
-// Réduire la navbar quand on descend vers le vite, la remettre à sa taille initiale si on remonte tout en haut
+// Réduire la navbar quand on descend vers le bas, la remettre à sa taille initiale si on remonte tout en haut
 
 // Faire apparaitre l'image de la partie improvise
 
 // Faire apparaitre la popup quand on est en bas du site
 
 // Bonus : quand on clicke sur la popup elle disparait pour toujours
+
+let playOnce = true;
+
+window.addEventListener("scroll", () => {
+  // console.log(window.scrollY);
+
+  if (window.scrollY > 50) {
+    navbar.style.height = "30px";
+  } else {
+    navbar.style.height = "90px";
+  }
+
+  let scrollValue =
+    ((window.scrollY + window.innerHeight) * 100) / document.body.offsetHeight;
+
+  if (scrollValue > 0.45) {
+    imgImprovise.style.opacity = 1;
+    imgImprovise.style.transform = "none";
+  }
+
+  if (scrollValue > 0.85 && playOnce) {
+    popup.style.opacity = 1;
+    popup.style.transform = "none";
+    playOnce = false;
+  }
+});
+
+closeBtn.addEventListener("click", () => {
+  popup.style.opacity = 0;
+  popup.style.transform = "translateX(500px)";
+});
+
+// 04m08s arrêt
