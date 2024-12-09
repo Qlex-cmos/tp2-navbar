278fa87672c04d3add0841b743f2c564fe46bc90
Qlex-cmos <alexcheula@gmail.com>
2024-12-09 22:50:39 +0100

    logique de génération d'un mdp

--- JS la Data P3/password-maker/index.js
+++ JS la Data P3/password-maker/index.js
@@ -4,23 +4,35 @@ const dataUppercase = dataLowercase.toUpperCase();
 const dataNumbers = "1234567890";
 const dataSymbols = 'ù!:;,"=)';
 // console.log(dataSymbols);
+const rangeValue = document.getElementById("password-length");
+// console.log(rangeValue.value);
+const passwordOutput = document.getElementById("password-output");
+console.log(passwordOutput);
 
 const generatePassword = () => {
   //   console.log("je génère un mot de passe");
   let data = [];
+  let password = "";
 
   //   console.log(lowercase.checked);
-  if (lowercase.checked) data.push(dataLowercase);
-  if (uppercase.checked) data.push(dataUppercase);
-  if (number.checked) data.push(dataNumbers);
-  if (symbols.checked) data.push(dataSymbols);
+  if (lowercase.checked) data.push(...dataLowercase);
+  if (uppercase.checked) data.push(...dataUppercase);
+  if (number.checked) data.push(...dataNumbers);
+  if (symbols.checked) data.push(...dataSymbols);
 
-  console.log(data);
+  if (data.length === 0) {
+    alert("Veuillez sélectionner des critères");
+    return; // return pour ne pas lire les instructions qui suivent
+  }
+
+  for (i = 0; i < rangeValue.value; i++) {
+    password += data[Math.floor(Math.random() * data.length)];
+    // console.log(password);
+  }
+  passwordOutput.value = password; // dans un input (ici passwordOutput -> voir index.html), on ne peut pas mettre de "texContent" ou de l"innerHtml"
 };
 
 generateButton.addEventListener("click", () => {
   //   console.log("test");
   generatePassword();
 });
-
-// 02:30:48
