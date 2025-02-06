9722a3267115fd31ff12a873340511c4178a59b5
Qlex-cmos <alexcheula@gmail.com>
2025-02-06 21:09:16 +0100

    user-app

--- JS API/Cours/index.js
+++ JS API/Cours/index.js
@@ -48,4 +48,59 @@ fetch("data.json", init).then((res) => console.log(res));
 
 // exemples BDD : mySQL, MONGOdB
 
-// 58:50
+// --------- METHODES POUR UTILISER JSON-SERVER (NODEJS) ------------
+// npm -v : version de npm
+// cd cours : changer de répertoire
+// npm init -y : initialisation des paquets npm -> donne package.json qui est un gestionnaire de dépendances (pour prendre des bibliothèques pour simuler un serveur)
+// npm i -g json-server : i pour install, g pour global, cela installe json-server
+// créer un fichier db.json
+// json-server --watch db.json
+
+// On va essayer maintenant de poster un message sur notre base de données
+const init2 = {
+  method: "POST", // ou DELETE et ajouter l'id du post à supprimer dans l'url du fetch
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({
+    pseudo: "Alex",
+    age: 42,
+  }),
+  mode: "cors",
+  credentials: "same-origin",
+};
+
+document.querySelector("form").addEventListener("submit", () => {
+  fetch("http://localhost:3000/users", init2).then(() =>
+    console.log("data envoyée")
+  );
+});
+
+// Ctrl+C pour arrêter le serveur
+//-----------------------------------------------------------------
+
+//--------------------------------
+// ASYNCHRONE : 3 METHODES
+// Permet de dire à Javascript "attend avant d'exécuter ce code" car JS est un langage de script qui parcours de haut en bas le code mais ne s'arrête pas spobntanément quand il faut attendre
+//--------------------------------
+
+// 1ere Méthode approximative
+setTimeout(() => {
+  console.log("test");
+}, 2000);
+// cette méthode manque de précision
+
+// 2e Méthode : PROMISE
+// fetch("monlien").then((res) => res);
+
+// 3e Méthode : ASYNC/AWAIT
+async function fetchData() {
+  await fetch("mon lien");
+  // attend que le await soit exécuté avant de faire la suite
+  executeFonction();
+}
+
+// version fonction fléchée de la méthode Async/await
+const fetchData2 = async () => {
+  // ensuite même logique que au dessus
+};
